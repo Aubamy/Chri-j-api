97073eecc23eeac83843c5e4919f794607240f06
Thompson3 <thompsoneno78@gmail.com>
2026-08-07 23:55:42 +0100

    vercel-fix

--- app.js
+++ app.js
@@ -15,7 +15,8 @@ app.use(cors({
     origin: [
         "http://localhost:5173",
         "http://localhost:5174",
-        "http://localhost:5175"
+        "http://localhost:5175",
+        "https://beckand-call-admin.vercel.app"
     ],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
