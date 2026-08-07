bc0a7cac0b5450c954d272a9e4aea60df8f24b30
Thompson3 <thompsoneno78@gmail.com>
2026-08-07 17:13:29 +0100

    cors-front-fix

--- app.js
+++ app.js
@@ -14,7 +14,8 @@ const cors = require("cors");
 app.use(cors({
     origin: [
         "http://localhost:5173",
-        "http://localhost:5174"
+        "http://localhost:5174",
+        "http://localhost:5175"
     ],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
